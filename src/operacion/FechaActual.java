473c604559032aba0d6b70ff3f05739eb41662e2
NicoDelRio <nicolas-delrio@hotmail.com>
2018-07-14 03:26:01 -0300

    TPA Asistente - Entrega 4
    Correcion Issues #4 y #5
    Un solo regex y sin usar while

--- src/operacion/FechaActual.java
+++ src/operacion/FechaActual.java
@@ -19,32 +19,48 @@ public class FechaActual implements Operacion{
 		
 		Fecha diaActual = new Fecha();
 		
-		String regex_DiaActual_Hora = ".*(?:que hora es|la hora).*";
-		Pattern pattern_DiaActual_Hora = Pattern.compile(regex_DiaActual_Hora, Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE);
-		Matcher matcher_DiaActual_Hora = pattern_DiaActual_Hora.matcher(pedido.getMensaje());
-		while(matcher_DiaActual_Hora.find()) {
-			if(matcher_DiaActual_Hora.matches()) {
-				return pedido.getNameUsuario() + " son las " + diaActual.hora(diaActual.formato_hora[0]);
+		String regex_DiaActual = ".*(que hora es|la hora|que dia es|la fecha|fecha es hoy|dia de la semana es|dia es de la semana).*";
+		Pattern pattern_DiaActual = Pattern.compile(regex_DiaActual, Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE);
+		Matcher matcher = pattern_DiaActual.matcher(pedido.getMensaje());
+		if(matcher.matches()) {
+			if(matcher.group(1).contains("que hora es") || matcher.group(1).contains("la hora")) {
+				return pedido.getNameUsuario() + " son las " + diaActual.hora(diaActual.formato_hora[0]);	
 			}
-		}
-
-		String regex_DiaActual_Fecha = ".*(?:que dia es|la fecha|fecha es hoy).*";
-		Pattern pattern_DiaActual_Fecha = Pattern.compile(regex_DiaActual_Fecha , Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE);
-		Matcher matcher_DiaActual_Fecha = pattern_DiaActual_Fecha.matcher(pedido.getMensaje());
-		while(matcher_DiaActual_Fecha.find()) {
-			if(matcher_DiaActual_Fecha.matches()) {
+			if(matcher.group(1).contains("que dia es") || matcher.group(1).contains("la fecha") || matcher.group(1).contains("fecha es hoy")) {
 				return pedido.getNameUsuario() + " hoy es " + diaActual.fechaToString(diaActual.formato_fecha[0]);
 			}
-		}
-
-		String regex_DiaActual_DiaSemana = ".*(?:dia de la semana es|dia es de la semana).*";
-		Pattern pattern_DiaActual_DiaSemana = Pattern.compile(regex_DiaActual_DiaSemana , Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE);
-		Matcher matcher_DiaActual_DiaSemana = pattern_DiaActual_DiaSemana.matcher(pedido.getMensaje());
-		while(matcher_DiaActual_DiaSemana.find()) {
-			if(matcher_DiaActual_DiaSemana.matches()) {
+			if(matcher.group(1).contains("dia de la semana es") || matcher.group(1).contains("dia es de la semana")) {
 				return pedido.getNameUsuario() + " hoy es " + diaActual.diaSemana();
 			}
+			
 		}
+		
+//		String regex_DiaActual_Hora = ".*(?:que hora es|la hora).*";
+//		Pattern pattern_DiaActual_Hora = Pattern.compile(regex_DiaActual_Hora, Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE);
+//		Matcher matcher_DiaActual_Hora = pattern_DiaActual_Hora.matcher(pedido.getMensaje());
+//		while(matcher_DiaActual_Hora.find()) {
+//			if(matcher_DiaActual_Hora.matches()) {
+//				return pedido.getNameUsuario() + " son las " + diaActual.hora(diaActual.formato_hora[0]);
+//			}
+//		}
+//
+//		String regex_DiaActual_Fecha = ".*(?:que dia es|la fecha|fecha es hoy).*";
+//		Pattern pattern_DiaActual_Fecha = Pattern.compile(regex_DiaActual_Fecha , Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE);
+//		Matcher matcher_DiaActual_Fecha = pattern_DiaActual_Fecha.matcher(pedido.getMensaje());
+//		while(matcher_DiaActual_Fecha.find()) {
+//			if(matcher_DiaActual_Fecha.matches()) {
+//				return pedido.getNameUsuario() + " hoy es " + diaActual.fechaToString(diaActual.formato_fecha[0]);
+//			}
+//		}
+//
+//		String regex_DiaActual_DiaSemana = ".*(?:dia de la semana es|dia es de la semana).*";
+//		Pattern pattern_DiaActual_DiaSemana = Pattern.compile(regex_DiaActual_DiaSemana , Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE);
+//		Matcher matcher_DiaActual_DiaSemana = pattern_DiaActual_DiaSemana.matcher(pedido.getMensaje());
+//		while(matcher_DiaActual_DiaSemana.find()) {
+//			if(matcher_DiaActual_DiaSemana.matches()) {
+//				return pedido.getNameUsuario() + " hoy es " + diaActual.diaSemana();
+//			}
+//		}
 		
 		return siguiente.calcular(pedido);
 	}
